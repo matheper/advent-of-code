5c6d3e9a86ec8e7af22edcde677dc3f1a9aab6db
Matheus Pereira <matheper@gmail.com>
2020-12-18 23:34:46 -0500

    Initilize computer system with DecoderV2.

--- 2020/day_14.py
+++ 2020/day_14.py
@@ -40,6 +40,53 @@ To initialize your ferry's docking program, you need the sum of all values left 
 Execute the initialization program. What is the sum of all values left in memory after it completes? (Do not truncate the sum to 36 bits.)
 
 
+--- Part Two ---
+For some reason, the sea port's computer system still can't communicate with your ferry's docking program. It must be using version 2 of the decoder chip!
+
+A version 2 decoder chip doesn't modify the values being written at all. Instead, it acts as a memory address decoder. Immediately before a value is written to memory, each bit in the bitmask modifies the corresponding bit of the destination memory address in the following way:
+
+If the bitmask bit is 0, the corresponding memory address bit is unchanged.
+If the bitmask bit is 1, the corresponding memory address bit is overwritten with 1.
+If the bitmask bit is X, the corresponding memory address bit is floating.
+A floating bit is not connected to anything and instead fluctuates unpredictably. In practice, this means the floating bits will take on all possible values, potentially causing many memory addresses to be written all at once!
+
+For example, consider the following program:
+
+mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1
+When this program goes to write to memory address 42, it first applies the bitmask:
+
+address: 000000000000000000000000000000101010  (decimal 42)
+mask:    000000000000000000000000000000X1001X
+result:  000000000000000000000000000000X1101X
+After applying the mask, four bits are overwritten, three of which are different, and two of which are floating. Floating bits take on every possible combination of values; with two floating bits, four actual memory addresses are written:
+
+000000000000000000000000000000011010  (decimal 26)
+000000000000000000000000000000011011  (decimal 27)
+000000000000000000000000000000111010  (decimal 58)
+000000000000000000000000000000111011  (decimal 59)
+Next, the program is about to write to memory address 26 with a different bitmask:
+
+address: 000000000000000000000000000000011010  (decimal 26)
+mask:    00000000000000000000000000000000X0XX
+result:  00000000000000000000000000000001X0XX
+This results in an address with three floating bits, causing writes to eight memory addresses:
+
+000000000000000000000000000000010000  (decimal 16)
+000000000000000000000000000000010001  (decimal 17)
+000000000000000000000000000000010010  (decimal 18)
+000000000000000000000000000000010011  (decimal 19)
+000000000000000000000000000000011000  (decimal 24)
+000000000000000000000000000000011001  (decimal 25)
+000000000000000000000000000000011010  (decimal 26)
+000000000000000000000000000000011011  (decimal 27)
+The entire 36-bit address space still begins initialized to the value 0 at every address, and you still need the sum of all values left in memory at the end of the program. In this example, the sum is 208.
+
+Execute the initialization program using an emulator for a version 2 decoder chip. What is the sum of all values left in memory after it completes?
+
+
 https://adventofcode.com/2020/day/14
 """
 
@@ -66,8 +113,40 @@ def initialize_program(instructions):
         else:
             memory[type_] = int(apply_mask(value, mask), 2)
             # mem[type_] = (int(value) & mask_and) | mask_or
-
     return sum(memory.values())
+
+
+class DecoderV2:
+    def __init__(self, instructions):
+        self.instructions = instructions
+        self._mask = None
+        self.memory = {}
+
+    def _parse_instructions(self):
+        for instruction in self.instructions:
+            type_, value = instruction.split(' = ')
+            if type_ == 'mask':
+                self._mask = value
+            else:
+                yield (f'{int(type_[4:-1]):036b}', int(value))
+
+    def _assign_value(self, address, value, index):
+        if index >= len(address):
+            int_address = int(''.join(address), 2)
+            self.memory[int_address] = value
+            return
+        if self._mask[index] == 'X':
+            address[index] = '0'
+            self._assign_value(address, value, index + 1)
+            address[index] = '1'
+        elif self._mask[index] == '1':
+            address[index] = '1'
+        self._assign_value(address, value, index + 1)
+
+    def initialize_program(self):
+        for address, value in self._parse_instructions():
+            self._assign_value(list(address), value, 0)
+        return sum(self.memory.values())
 
 def main():
     with open('inputs/day_14.txt') as input_file:
@@ -76,6 +155,9 @@ def main():
     memory = initialize_program(instructions)
     print(memory)
 
+    decoder = DecoderV2(instructions)
+    memory = decoder.initialize_program()
+    print(memory)
 
 if __name__ == '__main__':
     main()
