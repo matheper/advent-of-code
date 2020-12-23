ff84c0c88cff5e26bb4dfcfa3d6074bc3f93f16a
Matheus Pereira <matheper@gmail.com>
2020-12-22 22:30:12 -0500

    Reassembled back satellite`s camera array into a single image.

---
+++ 2020/day_20.py
@@ -0,0 +1,234 @@
+"""
+--- Day 20: Jurassic Jigsaw ---
+
+
+The high-speed train leaves the forest and quickly carries you south. You can even see a desert in the distance! Since you have some spare time, you might as well see if there was anything interesting in the image the Mythical Information Bureau satellite captured.
+
+After decoding the satellite messages, you discover that the data actually contains many small images created by the satellite's camera array. The camera array consists of many cameras; rather than produce a single square image, they produce many smaller square image tiles that need to be reassembled back into a single image.
+
+Each camera in the camera array returns a single monochrome image tile with a random unique ID number. The tiles (your puzzle input) arrived in a random order.
+
+Worse yet, the camera array appears to be malfunctioning: each image tile has been rotated and flipped to a random orientation. Your first task is to reassemble the original image by orienting the tiles so they fit together.
+
+To show how the tiles should be reassembled, each tile's image data includes a border that should line up exactly with its adjacent tiles. All tiles have this border, and the border lines up exactly when the tiles are both oriented correctly. Tiles at the edge of the image also have this border, but the outermost edges won't line up with any other tiles.
+
+For example, suppose you have the following nine tiles:
+
+Tile 2311:
+..##.#..#.
+##..#.....
+#...##..#.
+####.#...#
+##.##.###.
+##...#.###
+.#.#.#..##
+..#....#..
+###...#.#.
+..###..###
+
+Tile 1951:
+#.##...##.
+#.####...#
+.....#..##
+#...######
+.##.#....#
+.###.#####
+###.##.##.
+.###....#.
+..#.#..#.#
+#...##.#..
+
+Tile 1171:
+####...##.
+#..##.#..#
+##.#..#.#.
+.###.####.
+..###.####
+.##....##.
+.#...####.
+#.##.####.
+####..#...
+.....##...
+
+Tile 1427:
+###.##.#..
+.#..#.##..
+.#.##.#..#
+#.#.#.##.#
+....#...##
+...##..##.
+...#.#####
+.#.####.#.
+..#..###.#
+..##.#..#.
+
+Tile 1489:
+##.#.#....
+..##...#..
+.##..##...
+..#...#...
+#####...#.
+#..#.#.#.#
+...#.#.#..
+##.#...##.
+..##.##.##
+###.##.#..
+
+Tile 2473:
+#....####.
+#..#.##...
+#.##..#...
+######.#.#
+.#...#.#.#
+.#########
+.###.#..#.
+########.#
+##...##.#.
+..###.#.#.
+
+Tile 2971:
+..#.#....#
+#...###...
+#.#.###...
+##.##..#..
+.#####..##
+.#..####.#
+#..#.#..#.
+..####.###
+..#.#.###.
+...#.#.#.#
+
+Tile 2729:
+...#.#.#.#
+####.#....
+..#.#.....
+....#..#.#
+.##..##.#.
+.#.####...
+####.#.#..
+##.####...
+##..#.##..
+#.##...##.
+
+Tile 3079:
+#.#.#####.
+.#..######
+..#.......
+######....
+####.#..#.
+.#...#.##.
+#.#####.##
+..#.###...
+..#.......
+..#.###...
+By rotating, flipping, and rearranging them, you can find a square arrangement that causes all adjacent borders to line up:
+
+#...##.#.. ..###..### #.#.#####.
+..#.#..#.# ###...#.#. .#..######
+.###....#. ..#....#.. ..#.......
+###.##.##. .#.#.#..## ######....
+.###.##### ##...#.### ####.#..#.
+.##.#....# ##.##.###. .#...#.##.
+#...###### ####.#...# #.#####.##
+.....#..## #...##..#. ..#.###...
+#.####...# ##..#..... ..#.......
+#.##...##. ..##.#..#. ..#.###...
+
+#.##...##. ..##.#..#. ..#.###...
+##..#.##.. ..#..###.# ##.##....#
+##.####... .#.####.#. ..#.###..#
+####.#.#.. ...#.##### ###.#..###
+.#.####... ...##..##. .######.##
+.##..##.#. ....#...## #.#.#.#...
+....#..#.# #.#.#.##.# #.###.###.
+..#.#..... .#.##.#..# #.###.##..
+####.#.... .#..#.##.. .######...
+...#.#.#.# ###.##.#.. .##...####
+
+...#.#.#.# ###.##.#.. .##...####
+..#.#.###. ..##.##.## #..#.##..#
+..####.### ##.#...##. .#.#..#.##
+#..#.#..#. ...#.#.#.. .####.###.
+.#..####.# #..#.#.#.# ####.###..
+.#####..## #####...#. .##....##.
+##.##..#.. ..#...#... .####...#.
+#.#.###... .##..##... .####.##.#
+#...###... ..##...#.. ...#..####
+..#.#....# ##.#.#.... ...##.....
+For reference, the IDs of the above tiles are:
+
+1951    2311    3079
+2729    1427    2473
+2971    1489    1171
+To check that you've assembled the image correctly, multiply the IDs of the four corner tiles together. If you do this with the assembled tiles from the example above, you get 1951 * 3079 * 2971 * 1171 = 20899048083289.
+
+Assemble the tiles into an image. What do you get if you multiply together the IDs of the four corner tiles?
+
+
+
+https://adventofcode.com/2020/day/20
+"""
+
+class Camera:
+    def __init__(self, data):
+        self.tiles = self._parse(data)
+    
+    def _parse(self, data):
+        tiles = {}
+        for raw in data.split('\n\n'):
+            raw_tile = raw.split('\n')
+            id_ = int(''.join(raw_tile[0].split(' ')[1][:-1]))
+            tiles[id_] = raw_tile[1:]
+        return tiles
+
+    def _get_possible_borders(self, tile):
+        borders = [
+            tile[0],  # top
+            tile[-1],  # bottom
+            [t[0] for t in tile],  # left
+            [t[-1] for t in tile],  # right
+        ]
+        borders += [reversed(b) for b in borders]  # flip
+        borders = [''.join(b) for b in borders]
+        return borders
+
+    def _are_neighbors(self, tile_1, tile_2):
+        for b1 in self._get_possible_borders(tile_1):
+            for b2 in self._get_possible_borders(tile_2):
+                if b1 == b2:
+                    return True
+        return False
+
+    def _find_neighbors(self, tile):
+        neighbors = list()
+        for neighbor_key, neighbor_value in self.tiles.items():
+            if tile == neighbor_key:
+                continue
+            if self._are_neighbors(self.tiles[tile], neighbor_value):
+                neighbors.append(neighbor_key)
+        return len(neighbors)
+
+    def _find_corners(self):
+        corners = [
+            tile for tile in self.tiles
+            if self._find_neighbors(tile) == 2
+        ]
+        return corners
+
+    def corners_multiplication(self):
+        corners = self._find_corners()
+        result = 1
+        for corner in corners:
+            result *= corner
+        return result
+
+
+def main():
+    with open('inputs/day_20.txt') as input_file:
+        data = input_file.read()
+    camera = Camera(data)
+    print(camera.corners_multiplication())
+
+
+if __name__ == '__main__':
+    main()
