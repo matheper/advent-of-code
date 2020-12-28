6d3c351e42b18b4e87c8005317f52b6de8529c7e
Matheus Pereira <matheper@gmail.com>
2020-12-28 11:00:19 -0500

    Bringing life to the resort floor.

--- 2020/day_24.py
+++ 2020/day_24.py
@@ -43,25 +43,57 @@ In the above example, 10 tiles are flipped once (to black), and 5 more are flipp
 Go through the renovation crew's list and determine which tiles they need to flip. After all of the instructions have been followed, how many tiles are left with the black side up?
 
 
+--- Part Two ---
+The tile floor in the lobby is meant to be a living art exhibit. Every day, the tiles are all flipped according to the following rules:
+
+Any black tile with zero or more than 2 black tiles immediately adjacent to it is flipped to white.
+Any white tile with exactly 2 black tiles immediately adjacent to it is flipped to black.
+Here, tiles immediately adjacent means the six tiles directly touching the tile in question.
+
+The rules are applied simultaneously to every tile; put another way, it is first determined which tiles need to be flipped, then they are all flipped at the same time.
+
+In the above example, the number of black tiles that are facing up after the given number of days has passed is as follows:
+
+Day 1: 15
+Day 2: 12
+Day 3: 25
+Day 4: 14
+Day 5: 23
+Day 6: 28
+Day 7: 41
+Day 8: 37
+Day 9: 49
+Day 10: 37
+
+Day 20: 132
+Day 30: 259
+Day 40: 406
+Day 50: 566
+Day 60: 788
+Day 70: 1106
+Day 80: 1373
+Day 90: 1844
+Day 100: 2208
+After executing this process a total of 100 times, there would be 2208 black tiles facing up.
+
+How many tiles will be black after 100 days?
+
+
 https://adventofcode.com/2020/day/24
 """
 
-# How to navigate in an hexagon grid
+# How hexagon grids work
 # https://www.redblobgames.com/grids/hexagons/
+
+
+from copy import deepcopy
+
 
 class Floor:
     def __init__(self, data):
         self.tiles = data
-        self.pattern = dict()
-        # self._directions = {  # (z, y, x)
-        #     'e':  ( 1,  0),   # east
-        #     'se': ( 0,  1),   # southeast
-        #     'sw': (-1,  1),   # southwest
-        #     'w':  (-1,  0),   # west
-        #     'nw': ( 0, -1),   # northwest
-        #     'ne': ( 1, -1),   # northeast
-        # }
-        self._directions = {  # (z, y, x)
+        self.pattern = dict()     # {(z, y, x): n}
+        self._directions = {      # (z, y, x)
             'e':  ( 0, -1,  1),   # east
             'se': ( 1, -1,  0),   # southeast
             'sw': ( 1,  0, -1),   # southwest
@@ -81,22 +113,56 @@ class Floor:
             z, y, x = z + move[0], y + move[1], x + move[2]
         return (z, y, x)
 
-    def flit_tiles(self):
+    def arrange_tiles(self):
         for tile in self.tiles:
             position = self._get_position(tile)
-            self.pattern[position] = self.pattern.setdefault(position, 0) + 1
+            if position not in self.pattern:
+                self.pattern[position] = 1
+            else:
+                del self.pattern[position]
 
-    def count_black(self):
-        return sum([i%2 for i in self.pattern.values()])
+    def _get_neighbors(self, tile):
+        z, y, x = tile
+        neighbors = {
+            (z, y - 1, x + 1),  # east
+            (z + 1, y - 1, x),  # southeast
+            (z + 1, y, x - 1),  # southwest
+            (z, y + 1, x - 1),  # west
+            (z - 1, y + 1, x),  # northwest
+            (z - 1, y, x + 1),  # northeast
+        }
+        return neighbors
+
+    def flip_tiles(self, days):
+        for _ in range(days):
+            new_pattern = deepcopy(self.pattern)
+            black_tiles = set(self.pattern.keys())
+            for tile in self.pattern:
+                neighbors = self._get_neighbors(tile)
+                black_neighbors = neighbors.intersection(black_tiles)
+                white_neighbors = neighbors.difference(black_tiles)
+                if len(black_neighbors) not in (1, 2):
+                    del new_pattern[tile]
+                for w in white_neighbors:
+                    w_neighbors = self._get_neighbors(w)
+                    w_black_neighbors = w_neighbors.intersection(black_tiles)
+                    if len(w_black_neighbors) == 2:
+                        new_pattern[w] = 1
+            self.pattern = deepcopy(new_pattern)
+
+    def count_black_tiles(self):
+        return sum(self.pattern.values())
 
 def main():
     with open('inputs/day_24.txt') as input_file:
         data = input_file.read().splitlines()
+
     floor = Floor(data)
-    floor.flit_tiles()
-    print(floor.count_black())
-    # floor.play(100)
-    # print(floor.labels())
+    floor.arrange_tiles()
+    print(floor.count_black_tiles())
+
+    floor.flip_tiles(100)
+    print(floor.count_black_tiles())
 
 
 if __name__ == '__main__':
