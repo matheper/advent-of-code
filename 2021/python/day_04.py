d2d5b07e3a37e3239f763e173440f2ec5fcac2bb
Matheus Pereira <matheper@gmail.com>
2021-12-07 00:44:25 -0500

    Playing bingo

--- 2021/python/day_04.py
+++ 2021/python/day_04.py
@@ -1,17 +1,78 @@
+class Board:
+    def __init__(self, board_data):
+        rows = []
+        for line in board_data.split("\n"):
+            numbers = list(map(int, line.strip().split(" ")))
+            rows.append(numbers)
+        self.rows = rows
+        cols = []
+        for col_idx in range(len(self.rows[0])):
+            col = [row[col_idx] for row in self.rows]
+            cols.append(col)
+        self.cols = cols
+
+    def contains_line(self, sequence):
+        for row in self.rows:
+            if all(x in sequence for x in row):
+                return True
+        for col in self.cols:
+            if all(x in sequence for x in col):
+                return True
+        return False
+
+    def sum(self, sequence):
+        numbers = [x for row in self.rows for x in row]
+        marked = [x if x in numbers else 0 for x in sequence]
+        total = sum(numbers) - sum(marked)
+        return total
+
+
 def parse_input(input_data):
     data = input_data.replace("  ", " ").split("\n\n")
     sequence = list(map(int, data[0].split(",")))
     boards = []
     for board_data in data[1:]:
-        board = []
-        for line in board_data.split("\n"):
-            board.append(list(map(int, line.strip().split(" "))))
-        boards.append(board)
+        boards.append(Board(board_data))
     return sequence, boards
 
+
 def part_1(sequence, boards):
-    
+    drawn_values = []
     for value in sequence:
+        drawn_values.append(value)
+        for board in boards:
+            if board.contains_line(drawn_values):
+                return board.sum(drawn_values) * value
+    return -1
+
+
+def part_2(sequence, boards):
+    drawn_values = []
+    for value in sequence:
+        drawn_values.append(value)
+        idx = 0
+        while idx < len(boards):
+            board = boards[idx]
+            if board.contains_line(drawn_values):
+                last_board = boards.pop(idx)
+                if not len(boards):
+                    return last_board.sum(drawn_values) * value
+            else:
+                idx += 1
+    return -1
+
+
+def main():
+    with open(
+        "/Users/matheus/Projects/Algorithms/adventofcode/2021/inputs/day_04.txt"
+    ) as input_file:
+        input_data = input_file.read()
+    sequence, boards = parse_input(input_data)
+    winner_score = part_1(sequence, boards)
+    print(winner_score)
+
+    loser_score = part_2(sequence, boards)
+    print(loser_score)
 
 
 def test_day_04():
@@ -38,5 +99,10 @@ def test_day_04():
     winner_score = part_1(sequence, boards)
     assert winner_score == 4512
 
-if __name__ == '__main__':
-    test_day_04()
+    loser_score = part_2(sequence, boards)
+    assert loser_score == 1924
+
+
+if __name__ == "__main__":
+    main()
+    # test_day_04()
