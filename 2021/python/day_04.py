f79af10ceefef19691cc799aba28b927ad1b3786
Matheus Pereira <matheper@gmail.com>
2021-12-07 00:46:06 -0500

    Playing bingo

--- 2021/python/day_04.py
+++ 2021/python/day_04.py
@@ -64,7 +64,7 @@ def part_2(sequence, boards):
 
 def main():
     with open(
-        "/Users/matheus/Projects/Algorithms/adventofcode/2021/inputs/day_04.txt"
+        "../inputs/day_04.txt"
     ) as input_file:
         input_data = input_file.read()
     sequence, boards = parse_input(input_data)
@@ -75,34 +75,5 @@ def main():
     print(loser_score)
 
 
-def test_day_04():
-    input_data = """7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
-
-22 13 17 11  0
- 8  2 23  4 24
-21  9 14 16  7
- 6 10  3 18  5
- 1 12 20 15 19
-
- 3 15  0  2 22
- 9 18 13 17  5
-19  8  7 25 23
-20 11 10 24  4
-14 21 16 12  6
-
-14 21 17 24  4
-10 16 15  9 19
-18  8 23 26 20
-22 11 13  6  5
- 2  0 12  3  7"""
-    sequence, boards = parse_input(input_data)
-    winner_score = part_1(sequence, boards)
-    assert winner_score == 4512
-
-    loser_score = part_2(sequence, boards)
-    assert loser_score == 1924
-
-
 if __name__ == "__main__":
     main()
-    # test_day_04()
