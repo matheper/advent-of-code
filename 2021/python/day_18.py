584e26e393ca4ea5249c95db2143b8372066cd0f
Matheus Pereira <matheper@gmail.com>
2021-12-24 00:10:55 -0500

    Find the largest magnitude of pairs of snailfish numbers

--- 2021/python/day_18.py
+++ 2021/python/day_18.py
@@ -137,18 +137,35 @@ def magnitude(snailfish_numbers):
     right = 2 * magnitude(snailfish_numbers.right)
     return left + right
 
+
 def part_1(input_data):
     snailfish_numbers = [build_tree(i) for i in input_data]
     result = snailfish_numbers[0]
     for line in snailfish_numbers[1:]:
         result = snailfish_reduce(snailfish_sum(result, line))
-    print_snailfish(result)
-    print(magnitude(result))
+    # print_snailfish(result)
+    result = magnitude(result)
     return result
 
 
 def part_2(input_data):
-    pass
+    snailfish_numbers = [build_tree(i) for i in input_data]
+    results = []
+    for i in range(len(snailfish_numbers)):
+        for j in range(len(snailfish_numbers)):
+            if i != j:
+                results.append(
+                    magnitude(
+                        snailfish_reduce(
+                            snailfish_sum(
+                                snailfish_numbers[i],
+                                snailfish_numbers[j],
+                            )
+                        )
+                    )
+                )
+            snailfish_numbers = [build_tree(i) for i in input_data]
+    return max(results)
 
 
 def print_snailfish(value):
@@ -166,7 +183,7 @@ def main():
         input_data = parse(input_file)
 
     print(f"part_1: {part_1(input_data)}")
-    # print(f"part_2: {part_2(input_data)}")
+    print(f"part_2: {part_2(input_data)}")
 
 
 if __name__ == "__main__":
@@ -322,4 +339,28 @@ The final sum is:
 The magnitude of this final sum is 4140.
 
 Add up all of the snailfish numbers from the homework assignment in the order they appear. What is the magnitude of the final sum?
+
+
+--- Part Two ---
+You notice a second question on the back of the homework assignment:
+
+What is the largest magnitude you can get from adding only two of the snailfish numbers?
+
+Note that snailfish addition is not commutative - that is, x + y and y + x can produce different results.
+
+Again considering the last example homework assignment above:
+
+[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]
+The largest magnitude of the sum of any two snailfish numbers in this list is 3993. This is the magnitude of [[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]] + [[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]], which reduces to [[[[7,8],[6,6]],[[6,0],[7,7]]],[[[7,8],[8,8]],[[7,9],[0,6]]]].
+
+What is the largest magnitude of any sum of two different snailfish numbers from the homework assignment?
 """
